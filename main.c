ec8e2abe7f7ed5133c71876226784b03ebeb263c
Sergio Hernandez Castillo <shcastillo@MacBook-Pro.lan>
2020-08-18 23:06:58 -0500

    Se añadieron las estructuras necesarias.

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct {
+    char * nombre;
+    char * apellidos;
+    int edad;
+    char * telefono;
+    cama c;
+} paciente;
+
+typedef struct {
+    int id;
+    bool enUso = false;
+} cama;
+
+int main(int argc, const char * argv[]){
+    return 0;
+}
